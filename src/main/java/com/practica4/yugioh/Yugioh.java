9628df09cd88749d2c7402adb1f19a169b39aed7
GhostFir31 <j3sus0marle@gmail.com>
2023-11-10 02:02:07 -0800

    YUGI

---
+++ src/main/java/com/practica4/yugioh/Yugioh.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.practica4.yugioh;
+
+
+
+/**
+ *
+ * @author JesusOmar Leal
+ */
+public class Yugioh {
+
+    public static void main(String[] args)  {
+        
+        CollectionIDE ventana = new CollectionIDE();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);  
+
+        
+    }
+}
